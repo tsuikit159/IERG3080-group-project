eb337d868e0fa5fcae7f0d67277840ecdf2b445b
tsuikit159 <tsuikit159@gmail.com>
2023-12-20 16:37:15 +0800

    stage 1 : player movement
    TODO: apply physics rules into the player object and the sun object
    
    Make the player smaller when the ball is ejected
    
    Make the Collisions works

---
+++ Project/Project/Circle.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+/*  @Author: Tsui Man Kit 
+ *  @Purpose: Create a circle shape with circle factory 
+ *  @usage : double radius = 10;
+             IShapeFactory factory = new CircleFactory(radius);
+             Shape circle = factory.CreateShape();
+ */
+namespace Circle
+{
+    public interface IShapeFactory
+    {
+        Shape CreateShape();
+    }
+
+    public class CircleFactory : IShapeFactory
+    {
+        private double radius;
+
+        public CircleFactory(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public Shape CreateShape()
+        {
+            Ellipse circle = new Ellipse()
+            {
+                Width = radius * 2,
+                Height = radius * 2,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.Black 
+            };
+
+            return circle;
+        }
+    }
+}
--- Project/Project/MainWindow.xaml.cs
+++ Project/Project/MainWindow.xaml.cs
@@ -8,7 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using Circle;
 namespace Project
 {
     /// <summary>
@@ -16,9 +18,95 @@ namespace Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private DispatcherTimer gametimer = new DispatcherTimer();
+        private Canvas canvas;
+        private bool MousePressed = false;
+        private float speed = 2, speedx, speedy;
+        private MouseButtonEventArgs mouseEventArgs;
+        private int tickcount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            /// initzialize the canvas
+            /// 
+
+            canvas = new Canvas();
+
+            GameScreen.Focus();
+            Window.AddHandler(PreviewMouseLeftButtonDownEvent, new MouseButtonEventHandler(MouseDown), true);
+            Window.AddHandler(PreviewMouseLeftButtonUpEvent, new MouseButtonEventHandler(MouseUp), true);
+            timer.Interval = TimeSpan.FromMilliseconds(16);
+            timer.Tick += GameTick;
+            timer.Start();
+
+        }
+        private void GameTick(object sender, EventArgs e)
+        {
+            if (MousePressed == true)
+            {   // get the mouse position
+                tickcount ++;
+
+                float speed = 0.2f;
+                double centerX = mouseEventArgs.GetPosition(GameScreen).X;
+                double centerY = mouseEventArgs.GetPosition(GameScreen).Y;
+                // get the player position
+                double playerX = Canvas.GetLeft(Player);
+                double playerY = Canvas.GetTop(Player);
+                // get the angle between the mouse and the player
+                double dx = centerX - playerX;
+                double dy = centerY - playerY;
+                double angle = Math.Atan2(dy, dx);
+                double oppositeAngle = angle + Math.PI;
+                // move the player to the opposite side
+                double oppositeX = (playerX + Math.Cos(oppositeAngle));
+                double oppositeY = (playerY + Math.Sin(oppositeAngle));
+                // set the player position in the canvas
+                Canvas.SetLeft(Player, oppositeX);
+                Canvas.SetTop(Player, oppositeY);
+
+                if (tickcount > 8)
+                {
+                    tickcount = 0;
+                    // Code to execute when the gametimer reaches 500 milliseconds
+                    double radius = 10;
+                    IShapeFactory factory = new CircleFactory(radius);
+                    Shape circle = factory.CreateShape();
+                    // Move the circle to the opposite of the player side
+
+                    double circleX = (playerX - Math.Cos(oppositeAngle) - radius * 2);
+                    double circleY = (playerY - Math.Sin(oppositeAngle) - radius * 2);
+
+                    Canvas.SetLeft(circle, circleX);
+                    Canvas.SetTop(circle, circleY);
+                    GameScreen.Children.Add(circle);
+                }
+            }
+        }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                MousePressed = true;
+                mouseEventArgs = e;
+                
+                // set the circle position in the canvas
+                
+            }
+        }
+        public void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                MousePressed = false;
+                Console.WriteLine("mouse released");
+            }
         }
     }
+    
+
+
+    
 }
